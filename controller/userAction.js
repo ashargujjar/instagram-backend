b6eb25a681cdb976db5e55e6e7e06a1c69c96fc9
ashargujjar <asharmeelu@gmail.com>
2025-06-13 00:05:56 +0500

    DF

--- controller/userAction.js
+++ controller/userAction.js
@@ -44,22 +44,32 @@ exports.postComment = async (req, res, next) => {
     });
   }
 };
-
 exports.postLike = async (req, res, next) => {
   const postId = req.params.postId;
-  const resp = await Img.likePost(postId, req.user._id);
-  if (resp) {
-    return res.status(201).json({
-      msg: "user liked",
-    });
+  try {
+    const resp = await Img.likePost(postId, req.user._id);
+    if (resp.modifiedCount === 1) {
+      return res.status(201).json({ msg: "user liked" });
+    } else {
+      return res.status(400).json({ msg: "Already liked or post not found" });
+    }
+  } catch (err) {
+    console.error("Like Error:", err);
+    return res.status(500).json({ msg: "Server Error" });
   }
 };
+
 exports.removeLike = async (req, res, next) => {
   const postId = req.params.postId;
-  const resp = await Img.disLikePost(postId, req.user._id);
-  if (resp) {
-    return res.status(201).json({
-      msg: "user disliked",
-    });
+  try {
+    const resp = await Img.disLikePost(postId, req.user._id);
+    if (resp.modifiedCount === 1) {
+      return res.status(201).json({ msg: "user disliked" });
+    } else {
+      return res.status(400).json({ msg: "Post not found or not liked" });
+    }
+  } catch (err) {
+    console.error("Dislike Error:", err);
+    return res.status(500).json({ msg: "Server Error" });
   }
 };
