ab92c23bd1c9731717e52510bf8d0b14360cb7e0
ashargujjar <asharmeelu@gmail.com>
2025-06-12 23:04:31 +0500

    error in rouute fixed

--- routes/userAction.js
+++ routes/userAction.js
@@ -6,5 +6,5 @@ const router = express.Router();
 router.delete("/userPost/:id", is_auth, userAction.deleteUserPost);
 router.post("/postComment", is_auth, userAction.postComment);
 router.patch("/like/:postId", is_auth, userAction.postLike);
-router.patch("/disLike:/postId", is_auth, userAction.removeLike);
+router.patch("/disLike/:postId", is_auth, userAction.removeLike);
 module.exports = router;
